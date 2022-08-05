0405ff1bc44235c6224b59492a4bf9a327bd5535
MarkoAyala <markoayala3@hotmail.com>
2022-08-05 17:58:57 -0300

    change stock model

--- src/interfaces/Stock.d.ts
+++ src/interfaces/Stock.d.ts
@@ -22,9 +22,34 @@ export interface StockInterface {
     all:number | 0;
 }
 
+interface Red {
+    red:{
+        code:string
+        stock_red:number | 0;
+        xs:number | 0;
+        s:number | 0;
+        m:number | 0;
+        l:number | 0;
+        xl:number | 0;
+        xxl:number | 0;
+    }
+}
+interface Yellow{
+    yellow:{
+        code:string
+        stock_yellow:number | 0;
+        xs:number | 0;
+        s:number | 0;
+        m:number | 0;
+        l:number | 0;
+        xl:number | 0;
+        xxl:number | 0;
+    },
+}
+
 
 export interface StockMapped{
     _id:string;
     name:string;
-    stock:StockInterface
+    stock:Array<Array<Red>,Array<Yellow>>
 }
