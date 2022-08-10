23eb68b1d533147ea9c1f6850594b9918e36d61e
MarkoAyala <markoayala3@hotmail.com>
2022-08-10 02:45:58 -0300

    editStock suma los stocks y total

--- src/controllers/Stock/editStock.ts
+++ src/controllers/Stock/editStock.ts
@@ -7,14 +7,25 @@ export const EDIT_STOCK = async (
     res:Response,
     _next:NextFunction
 ) => {
-    const { id , name , stock} = req.body;
+    const { _id , name , stock} = req.body;
     try {
-        if(!id){
+        if(!_id || !stock){
             throw new Error('Especifique el id del stock que quiere editar');
         }
-        const oldStock = await StockModel.updateOne({_id:id},{
+        let hare = stock.map((element:any)=>{
+            for(let property in element[0]){
+                if(property !== 'all'){
+                    return element[0][property] =[{[property]:{...element[0][property], [`stock_${property}`]:element[0][property].xs + element[0][property].s + element[0][property].m + element[0][property].l + element[0][property].xl + element[0][property].xxl}}]
+                }else{
+                    return element
+                }
+            }
+        })
+        const number = hare[0][0].red.stock_red + hare[1][0].yellow.stock_yellow
+        hare[2][0].all = number
+        const oldStock = await StockModel.updateOne({_id:_id},{
             name: name && name,
-            stock: stock && stock
+            stock: hare && hare
         });
         if(oldStock){
             res.status(200).json(oldStock);
