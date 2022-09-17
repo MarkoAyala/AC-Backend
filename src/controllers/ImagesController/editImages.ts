54576d1df5b4b91a3fc86f6dce0e226b5efd23fe
MarkoAyala <markoayala3@hotmail.com>
2022-09-16 21:25:16 -0300

    delete images in images controller

--- src/controllers/ImagesController/editImages.ts
+++ src/controllers/ImagesController/editImages.ts
@@ -1,4 +1,5 @@
 import { NextFunction, Request, Response } from "express";
+import { destroyMultimedia } from "../../cloudinary";
 import { ImagesModel } from "../../models/Images";
 
 
@@ -16,11 +17,17 @@ export const EDIT_IMAGES = async (
             if(!name){
                 throw new Error('Ha ocurrido un error al editar image');
             }else{
-                const imageEdited = await ImagesModel.updateOne({name:name},{
-                    url: url && url,
-                    public_id:public_id && public_id
-                });
-                res.status(200).json(imageEdited);
+                const DeleteImage = await ImagesModel.findOne({name:name});
+                if(DeleteImage){
+                    const imageEdited = await ImagesModel.updateOne({name:name},{
+                        url: url && url,
+                        public_id:public_id && public_id
+                    });
+                    destroyMultimedia(DeleteImage?.public_id)
+                    res.status(200).json(imageEdited);
+                }else{
+                    throw new Error('No se encuentra la imagen');
+                }
             }
         }catch(err){
             throw new Error('No se ha encontrado la imagen existente con el id ingresado');
