a8a1a3eb64f5d932891e48686ee39dc207f2f2d0
MarkoAyala <markoayala3@hotmail.com>
2022-10-06 00:50:54 -0300

    payment mail

--- src/controllers/PaymentController/PaymentNotification.ts
+++ src/controllers/PaymentController/PaymentNotification.ts
@@ -15,18 +15,18 @@ export const PAYMENT_NOTIFICATION = async (
   try {
     const {id , topic } = req.query
     let info:any;
+    let resultado:any;
     if(topic === 'payment'){
       info = await(await axios.get(`https://api.mercadopago.com/v1/payments/${id}`)).data
-      console.log(info);
-        await transporter.sendMail({
+        resultado = await transporter.sendMail({
           from: '"ALTO CUERO - Información de contacto" <markoayala147@gmail.com>', 
-          to: info.payer.last_name, 
-          subject: "Gracias por tu compra 🧡", // Subject line
-          text: "Hello world?", // plain text body
+          to: `${info.payer.last_name}`, 
+          subject: "Gracias por tu compra 🧡",
           html: `<div style={{margin:20px auto}}>   <p>Hola ${info.payer.first_name}!! , nos comunicamos para decirte que tu compra fue exitosa. A continuación te brindamos informacion nuestra para que estes en contacto:</p></br><p>Escribinos a nuestro WhatsApp con tu ID de compra para acelerar el proceso y poder hacer el envio lo antes posible --> ID: ${id}, nuestro whatsapp:+54 11700995411. Muchas gracias por tu compra <3!</p>                </div>`, // html body
         });
     }
-   res.status(200).json(info);
+    console.log("RESULTADO", resultado);
+   res.status(200).json(resultado);
   } catch (error: string | any) {
     res.status(400).json(`Error en el controller PAYMENT : ${error}`);
   }
