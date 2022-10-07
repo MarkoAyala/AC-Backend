8564d4c14b31251ca0c30c3c79c788b8d51ee42f
MarkoAyala <markoayala3@hotmail.com>
2022-10-07 20:53:28 -0300

    no anda

--- src/controllers/PaymentController/PaymentNotification.ts
+++ src/controllers/PaymentController/PaymentNotification.ts
@@ -18,17 +18,17 @@ export const PAYMENT_NOTIFICATION = async (
     let resultado:any;
     if(id){//cambiar a merchant
       console.log("entre")
-      info = await(await axios.get(`https://api.mercadopago.com/v1/payments/${id}`)).data
+      info = await axios.get(`https://api.mercadopago.com/v1/payments/${id}`)
         resultado = await transporter.sendMail({
           from: '"ALTO CUERO - Información de contacto" <markoayala147@gmail.com>', 
-          to: `${info.payer.last_name}`, 
+          to: `${info.data.payer.last_name}`, 
           subject: "Gracias por tu compra 🧡",
-          html: `<div style={{margin:20px auto}}>   <p>Hola ${info.payer.first_name}!! , nos comunicamos para decirte que tu compra fue exitosa. A continuación te brindamos informacion nuestra para que estes en contacto:</p></br><p>Escribinos a nuestro WhatsApp con tu ID de compra para acelerar el proceso y poder hacer el envio lo antes posible --> ID: ${id}, nuestro whatsapp:+54 11700995411. Muchas gracias por tu compra <3!</p>                </div>`, // html body
+          html: `<div style={{margin:20px auto}}>   <p>Hola ${info.data.payer.first_name}!! , nos comunicamos para decirte que tu compra fue exitosa. A continuación te brindamos informacion nuestra para que estes en contacto:</p></br><p>Escribinos a nuestro WhatsApp con tu ID de compra para acelerar el proceso y poder hacer el envio lo antes posible --> ID: ${id}, nuestro whatsapp:+54 11700995411. Muchas gracias por tu compra <3!</p>                </div>`, // html body
         });
     }
     console.log("INFO", info); // probar si sale que fue aprobado
     console.log("RESULTADO", resultado);
-    console.log("id",typeof id , id);
+    console.log("id",req.query);
    res.status(200).json(resultado);
   } catch (error: string | any) {
     res.status(400).json(`Error en el controller PAYMENT : ${error}`);
