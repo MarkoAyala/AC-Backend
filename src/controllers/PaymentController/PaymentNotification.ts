35bb5c233318e3157add86c3e7c7a18902f3f433
MarkoAyala <markoayala3@hotmail.com>
2022-10-10 19:56:40 -0300

    payment20

--- src/controllers/PaymentController/PaymentNotification.ts
+++ src/controllers/PaymentController/PaymentNotification.ts
@@ -31,8 +31,7 @@ export const PAYMENT_NOTIFICATION = async (
             to: payment.additional_info?.payer.last_name, 
             subject: `Gracias por tu compra ${payment.additional_info?.payer.first_name} 🧡`,
             html: `<div style={{margin:20px auto}}>   <p>Hola !! , nos comunicamos para decirte que tu compra fue exitosa. A continuación te brindamos informacion nuestra para que estes en contacto:</p></br><p>Escribinos a nuestro WhatsApp con tu ID de compra para acelerar el proceso y poder hacer el envio lo antes posible --> ID: , nuestro whatsapp:+54 11700995411. Muchas gracias por tu compra <3!</p>                </div>`, // html body
-          });
-          console.log('EMAIL', email);
+          }).then((res)=> console.log("email", res))
           res.status(201).json({email});
       }else{
         console.log('nocreado')
