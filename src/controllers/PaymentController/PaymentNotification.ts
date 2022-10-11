a93e1d45fa3b2009acb1e5880fd0254a88fc0cd8
MarkoAyala <markoayala3@hotmail.com>
2022-10-11 11:21:59 -0300

    payment final

--- src/controllers/PaymentController/PaymentNotification.ts
+++ src/controllers/PaymentController/PaymentNotification.ts
@@ -30,10 +30,29 @@ export const PAYMENT_NOTIFICATION = async (
             from: '"ALTO CUERO - Información de contacto" <markoayala147@gmail.com>', 
             to: payment.additional_info?.payer.last_name, 
             subject: `Gracias por tu compra ${payment.additional_info?.payer.first_name} 🧡`,
-            html: `<div style={{margin:20px auto}}>   <p>Hola !! , nos comunicamos para decirte que tu compra fue exitosa. A continuación te brindamos informacion nuestra para que estes en contacto:</p></br><p>Escribinos a nuestro WhatsApp con tu ID de compra para acelerar el proceso y poder hacer el envio lo antes posible --> ID: , nuestro whatsapp:+54 11700995411. Muchas gracias por tu compra <3!</p>                </div>`, // html body
+            html: `
+            <div style={{width:'100%', backgroundColor:'#242424', color:'white', overflow:'hidden'}}>
+                        <div style={{width:'100%', display:'flex'}}>
+
+                        <img src={Banner} alt="no img" style={{width:'95%', height:'150px', objectFit:'cover', margin:'0px auto 0px auto', borderRadius:'7px'}} />
+                        </div>
+                        <h3 style={{textAlign:'center', margin:'15px 0px'}}>Gracias por confiar en nosotros,${payment.additional_info?.payer.first_name}</h3>
+                        <div style={{margin:'0px 20px', border:'2px solid #555', padding:'20px 10px'}}>
+                            <p>Para mantenernos en contacto, mandanos un mensaje para avisarnos de la compra enviando el codigo del comprobante de pago <span style={{border:'1px solid gray', padding:'0px 15px', backgroundColor:'#060606'}}>${id}</span> para acelerar el proceso a este numero de WhatsApp: +54 1170995411 </p>
+                            <h4 style={{marginTop:'10px'}}>Los datos del envio son los siguientes:</h4>
+                            <ul style={{marginLeft:'30px'}}>
+                                <li> <span style={{borderBottom:'2px solid #555'}}>Calle:</span> ${payment.additional_info?.payer.address.street_name}</li>
+                                <li> <span style={{borderBottom:'2px solid #555'}}>Numeración:</span> ${payment.additional_info?.payer.address.street_number}</li>
+                                <li> <span style={{borderBottom:'2px solid #555'}}>C.Postal:</span> ${payment.additional_info?.payer.address.zip_code}</li>
+                            </ul>
+                            <h4 style={{color:'#ffb100', margin:'10px 0px 0px 0px'}}>En caso de que alguno de estos datos sean erroneos o quieras darnos mas detalles sobre el envio porfavor cumunicate con nosotros lo antes posible.</h4>
+                            <h4 style={{color:'rgb(87 177 255)', margin:'10px 0px 0px 0px', textAlign:'center'}}>Tu mensaje no es molestia 🙂</h4>
+                        </div>
+            </div>
+            
+            `,
           }).then((response)=>{
             if(response.accepted[0]){
-              console.log(payment)
               res.status(201).json({email:'terminado'});
             }else{
               throw new Error('No se envio a ningun email');
